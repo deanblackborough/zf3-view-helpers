43d6ddbe9658771ee3decc2f23d3c9ae542535e2
Dean Blackborough <dean@g3d-development.com>
2017-05-22 22:19:19 +0100

    Badge
    * Bootstrap 3 badge

---
+++ src/Bootstrap3Badge.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types=1);
+
+namespace DBlackborough\Zf3ViewHelpers;
+
+use Zend\View\Helper\AbstractHelper;
+
+/**
+ * Generate a Bootstrap 3 badge
+ *
+ * @package DBlackborough\Zf3ViewHelpers
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright Dean Blackborough
+ * @license https://github.com/deanblackborough/zf3-view-helpers/blob/master/LICENSE
+ */
+class Bootstrap3Badge extends AbstractHelper
+{
+    /**
+     * @var string Button label
+     */
+    private $label;
+
+    /**
+     * Entry point for the view helper
+     *
+     * @param string $label
+     *
+     * @return Bootstrap3Badge
+     */
+    public function __invoke(string $label): Bootstrap3Badge
+    {
+        $this->reset();
+
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * Reset all properties in case the view helper is called multiple times within a script
+     *
+     * @return void
+     */
+    private function reset(): void
+    {
+        $this->label = null;
+    }
+
+    /**
+     * Worker method for the view helper, generates the HTML, the method id private so that we
+     * can echo/print the view helper directly
+     *
+     * @return string
+     */
+    private function render(): string
+    {
+        return '<span class="badge">' . $this->label . '</span>';
+    }
+
+    /**
+     * Override the __toString() method to allow echo/print of the view helper directly,
+     * saves a call to render()
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->render();
+    }
+}
