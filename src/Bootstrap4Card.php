f666fd1f64351a5e03a27cade3534c9fb31e2d65
Dean Blackborough <dean@g3d-development.com>
2017-08-16 01:03:04 +0100

    Card
    * Developing initial version of card component

---
+++ src/Bootstrap4Card.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace DBlackborough\Zf3ViewHelpers;
+
+use Zend\View\Helper\AbstractHelper;
+
+/**
+ * Generate a Bootstrap 4 card component
+ *
+ * @package DBlackborough\Zf3ViewHelpers
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright Dean Blackborough
+ * @license https://github.com/deanblackborough/zf3-view-helpers/blob/master/LICENSE
+ */
+class Bootstrap4Card extends AbstractHelper
+{
+    
+}
