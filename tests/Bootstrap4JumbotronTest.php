c5fa71cd74cd325198e1ab2242bfdf706512b044
Dean Blackborough <dean@g3d-development.com>
2017-10-10 17:38:21 +0100

    Bg and text style
    * Initial test for bg style and text style

--- tests/Bootstrap4JumbotronTest.php
+++ tests/Bootstrap4JumbotronTest.php
@@ -33,4 +33,102 @@ final class Bootstrap4JumbotronTest extends PHPUnit\Framework\TestCase
             $view_helper->__toString()
         );
     }
+
+    /**
+     * Add a sub heading
+     */
+    public function testFluidOption()
+    {
+        $view_helper = new Bootstrap4Jumbotron();
+        $view_helper->__invoke('Heading', '<p>Content</p>')
+            ->fluid();
+        $this->assertEquals(
+            '<div class="jumbotron jumbotron-fluid"><div class="container"><h1 class="display-1">Heading</h1><p>Content</p></div></div>',
+            $view_helper->__toString()
+        );
+    }
+
+    /**
+     * Heading display-1
+     */
+    public function testHeadingDisplay1()
+    {
+        $view_helper = new Bootstrap4Jumbotron();
+        $view_helper->__invoke('Heading', '<p>Content</p>')
+            ->headingDisplayLevel(1);
+        $this->assertEquals(
+            '<div class="jumbotron"><h1 class="display-1">Heading</h1><p>Content</p></div>',
+            $view_helper->__toString()
+        );
+    }
+
+    /**
+     * Heading display-2
+     */
+    public function testHeadingDisplay2()
+    {
+        $view_helper = new Bootstrap4Jumbotron();
+        $view_helper->__invoke('Heading', '<p>Content</p>')
+            ->headingDisplayLevel(2);
+        $this->assertEquals(
+            '<div class="jumbotron"><h1 class="display-2">Heading</h1><p>Content</p></div>',
+            $view_helper->__toString()
+        );
+    }
+
+    /**
+     * Heading display-3
+     */
+    public function testHeadingDisplay3()
+    {
+        $view_helper = new Bootstrap4Jumbotron();
+        $view_helper->__invoke('Heading', '<p>Content</p>')
+            ->headingDisplayLevel(3);
+        $this->assertEquals(
+            '<div class="jumbotron"><h1 class="display-3">Heading</h1><p>Content</p></div>',
+            $view_helper->__toString()
+        );
+    }
+
+    /**
+     * Heading display-4
+     */
+    public function testHeadingDisplay4()
+    {
+        $view_helper = new Bootstrap4Jumbotron();
+        $view_helper->__invoke('Heading', '<p>Content</p>')
+            ->headingDisplayLevel(4);
+        $this->assertEquals(
+            '<div class="jumbotron"><h1 class="display-4">Heading</h1><p>Content</p></div>',
+            $view_helper->__toString()
+        );
+    }
+
+    /**
+     * Set background style primary
+     */
+    public function testBgStylePrimary()
+    {
+        $view_helper = new Bootstrap4Jumbotron();
+        $view_helper->__invoke('Heading', '<p>Content</p>')
+            ->bgStyle('primary');
+        $this->assertEquals(
+            '<div class="jumbotron bg-primary"><h1 class="display-1">Heading</h1><p>Content</p></div>',
+            $view_helper->__toString()
+        );
+    }
+
+    /**
+     * Set text style primary
+     */
+    public function testTextStylePrimary()
+    {
+        $view_helper = new Bootstrap4Jumbotron();
+        $view_helper->__invoke('Heading', '<p>Content</p>')
+            ->textStyle('primary');
+        $this->assertEquals(
+            '<div class="jumbotron text-primary"><h1 class="display-1">Heading</h1><p>Content</p></div>',
+            $view_helper->__toString()
+        );
+    }
 }
