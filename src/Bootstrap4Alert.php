bf3cc5acd8c62813cd4e41e488851ea21f299224
Dean Blackborough <dean@g3d-development.com>
2017-12-01 00:46:53 +0000

    Alert view helper
    * Tiny bit more work on the alert viw helper
    * Removed redundant field in button, also, uri was not being reset
    * Type hints missing

--- src/Bootstrap4Alert.php
+++ src/Bootstrap4Alert.php
@@ -26,6 +26,36 @@ class Bootstrap4Alert extends Bootstrap4Helper
     }
 
     /**
+     * Set the background colour for the component, needs to be one of the following, primary, secondary, success,
+     * danger, warning, info, light, dark or white, if an incorrect style is passed in we don't apply the class.
+     *
+     * @param string $color
+     *
+     * @return Bootstrap4Alert
+     */
+    public function setBgStyle(string $color) : Bootstrap4Alert
+    {
+        $this->assignBgStyle($color);
+
+        return $this;
+    }
+
+    /**
+     * Set the text color for the component, need to be one of the following, primary, secondary, success, danger,
+     * warning, info, light or dark, if an incorrect style is passed in we don't apply the class.
+     *
+     * @param string $color
+     *
+     * @return Bootstrap4Alert
+     */
+    public function setTextStyle(string $color) : Bootstrap4Alert
+    {
+        $this->assignTextStyle($color);
+
+        return $this;
+    }
+
+    /**
      * Worker method for the view helper, generates the HTML, the method is private so that we
      * can echo/print the view helper directly
      *
@@ -34,6 +64,26 @@ class Bootstrap4Alert extends Bootstrap4Helper
     protected function render(): string
     {
         return '';
+    }
+
+    /**
+     * Generate the classes for the alert component based on the set options
+     *
+     * @return string
+     */
+    private function classes(): string
+    {
+        $classes = '';
+
+        if ($this->bg_color !== null) {
+            $classes = ' alert-' . $this->bg_color;
+        }
+
+        if ($this->text_color !== null) {
+            $classes .= ' text-' . $this->text_color;
+        }
+
+        return $classes;
     }
 
     /**
